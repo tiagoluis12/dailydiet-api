b69488e429926394a2ace99f288d7a3cf3e870ab
“tiagoluis12” <tiago7164@hotmail.com>
2023-11-11 00:51:47 +0000

    Implemented: Project structure and database

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import fastify from 'fastify'
+import crypto from 'node:crypto'
+import { knex } from './database'
+import { env } from './env'
+
+const app = fastify()
+
+app.get('/hello', async () => {
+  const transactions = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
+
+  return transactions
+})
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('Server is running on port 3333')
+  })
