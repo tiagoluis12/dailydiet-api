145465058fb345ef819bf52cb8b7638d64696ac0
“tiagoluis12” <tiago7164@hotmail.com>
2023-11-15 21:04:27 +0000

    Chore: Production Config

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { env } from './env'
 app
   .listen({
     port: env.PORT,
+    host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
   })
   .then(() => {
     console.log('Server is running on port 3333')
